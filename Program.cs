9241e3a14249c62a6d25ba621f4f195ee3eca7d8
Aishu030 <aishuramesh031@gmail.com>
2023-07-27 15:48:33 +0530

    commit

--- Program.cs
+++ Program.cs
@@ -11,14 +11,12 @@ namespace Assignment17
         static void Main(string[] args)
         {
 
-            Circle circle = new Circle(5);
-            Rectangle rectangle = new Rectangle(4, 6);
-            Triangle triangle = new Triangle(3, 4, 5);
-    
+            Circle circle = new Circle(14);
             circle.PrintInfo();
+            Rectangle rectangle = new Rectangle(2.3, 11);
             rectangle.PrintInfo();
+            Triangle triangle = new Triangle(33, 44, 55);
             triangle.PrintInfo();
-
             Console.WriteLine($"Circle -  Area: {circle.Area}, \t\tPerimeter: {circle.Perimeter}\n");
             Console.WriteLine($"Rectangle - Area: {rectangle.Area}, \t\t Perimeter: {rectangle.Perimeter}\n");
             Console.WriteLine($"Triangle -  Area: {triangle.Area}, \t\t Perimeter: {triangle.Perimeter}");
